c56667d1346c5c22e27da2bc25891a240b7ec08e
Rafael Corrêa Gomes <rafaelcgstz@gmail.com>
2019-08-07 23:58:53 -0300

    [ZAIR-138] Checking error 500 > Adding retry feature

--- src/Zaius/Worker.php
+++ src/Zaius/Worker.php
@@ -2,19 +2,26 @@
 
 namespace ZaiusSDK\Zaius;
 
-class Worker extends \DJWorker {
+use ZaiusSDK\ZaiusException;
+
+class Worker extends \DJWorker
+{
     public function __construct(array $options = array())
     {
-        if(!isset($options['sleep'])) {
+        if (!isset($options['sleep'])) {
             $options['sleep'] = 0;
         }
+        if (!isset($options["max_attempts"])) {
+            $options['max_attempts'] = 1;
+        }
+
         parent::__construct($options);
     }
 
-    public function processAll() {
-        while($job = self::getNewJob('default')) {
+    public function processAll()
+    {
+        while ($job = self::getNewJob('default')) {
             $job->run();
         }
     }
-
 }
